758fbaee4bff47b50232d1226d88959502cd5f0c
Ansh gajbhiye <111952775+Anshmangithub@users.noreply.github.com>
2024-07-22 01:37:07 +0530

    counter in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 
-
+import { useState } from "react"
 
 
 function App() {
+  const [counter, setcounter] = useState(0)
+
+  const increment = ()=>{
+
+    if(counter < 20){
+      setcounter(counter + 1);
+    }
+    
+  }
+  const decrement = (e)=>{
+ 
+    if(counter > 0){
+      setcounter(counter - 1);
+    }
+    
+  }
+
 
   return (
     <>
-     <h1>hello i am ansh</h1>
+    <div className="mb-">
+      <h2>{counter}</h2>
+    <button className="btn btn-primary" onClick={increment}>Add counter</button>
+    <button  className="btn btn-danger" onClick={decrement}>remove counter</button>
+    </div>
     </>
   )
 }
